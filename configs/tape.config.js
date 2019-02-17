1582b38b45e2fe218700d45de01154bb6041ca8d
Philip Bordallo <git@philipbordallo.com>
2019-02-17 11:28:14 -0800

    Add initial configs

---
+++ configs/tape.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'basic': {
+    message: 'should support basic usage',
+    options: {},
+  },
+
+  'custom-properties': {
+    message: 'should support being used with custom properties',
+    options: {},
+  },
+};
